1d6e6c37181cf1dfb910d251fbc096dceec6d352
Tom Coleman <tom@thesnail.org>
2013-04-07 16:58:57 +1000

    Added basic posts list template and static data.

---
+++ client/views/posts/posts_list.js
@@ -0,0 +1,20 @@
+var postsData = [
+  {
+    title: 'Introducing Telescope',
+    author: 'Sacha Greif',
+    url: 'http://sachagreif.com/introducing-telescope/'
+  }, 
+  {
+    title: 'Meteor',
+    author: 'Tom Coleman',
+    url: 'http://meteor.com'
+  }, 
+  {
+    title: 'The Meteor Book',
+    author: 'Tom Coleman',
+    url: 'http://themeteorbook.com'
+  }
+];
+Template.postsList.helpers({
+  posts: postsData
+});
