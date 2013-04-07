abd1c279cc76f9a53588c73c19b8247baee272be
Tom Coleman <tom@thesnail.org>
2013-04-07 19:25:58 +1000

    Use a method to submit the post.
    chapter7-6

--- collections/posts.js
+++ collections/posts.js
@@ -1,8 +1,34 @@
 Posts = new Meteor.Collection('posts');
 
-Posts.allow({
-  insert: function(userId, doc) {
-    // only allow posting if you are logged in
-    return !! userId;
+Meteor.methods({
+  post: function(postAttributes) {
+    var user = Meteor.user(),
+      postWithSameLink = Posts.findOne({url: postAttributes.url});
+    
+    // ensure the user is logged in
+    if (!user)
+      throw new Meteor.Error(401, "You need to login to post new stories");
+    
+    // ensure the post has a title
+    if (!postAttributes.title)
+      throw new Meteor.Error(422, 'Please fill in a headline');
+    
+    // check that there are no previous posts with the same link
+    if (postAttributes.url && postWithSameLink) {
+      throw new Meteor.Error(302, 
+        'This link has already been posted', 
+        postWithSameLink._id);
+    }
+    
+    // pick out the whitelisted keys
+    var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), {
+      userId: user._id, 
+      author: user.username, 
+      submitted: new Date().getTime()
+    });
+    
+    var postId = Posts.insert(post);
+    
+    return postId;
   }
 });
