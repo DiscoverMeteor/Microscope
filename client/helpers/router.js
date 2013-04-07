575662f97a6a4562530a3d9cc44cef580b1d58aa
Tom Coleman <tom@thesnail.org>
2013-04-07 19:09:55 +1000

    Show a loading screen while waiting to login.

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -13,6 +13,8 @@ Meteor.Router.filters({
   'requireLogin': function(page) {
     if (Meteor.user())
       return page;
+    else if (Meteor.loggingIn())
+      return 'loading';
     else
       return 'accessDenied';
   }
