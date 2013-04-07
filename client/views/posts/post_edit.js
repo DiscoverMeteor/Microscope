884f88bcbf29a3a4d852805bf833fd930cbb4331
Tom Coleman <tom@thesnail.org>
2013-04-07 23:14:36 +1000

    Added edit posts form

---
+++ client/views/posts/post_edit.js
@@ -0,0 +1,37 @@
+Template.postEdit.helpers({
+  post: function() {
+    return Posts.findOne(Session.get('currentPostId'));
+  }
+});
+
+Template.postEdit.events({
+  'submit form': function(e) {
+    e.preventDefault();
+    
+    var currentPostId = Session.get('currentPostId');
+    
+    var postProperties = {
+      url: $(e.target).find('[name=url]').val(),
+      title: $(e.target).find('[name=title]').val()
+    }
+    
+    Posts.update(currentPostId, {$set: postProperties}, function(error) {
+      if (error) {
+        // display the error to the user
+        alert(error.reason);
+      } else {
+        Meteor.Router.to('postPage', currentPostId);
+      }
+    });
+  },
+  
+  'click .delete': function(e) {
+    e.preventDefault();
+    
+    if (confirm("Delete this post?")) {
+      var currentPostId = Session.get('currentPostId');
+      Posts.remove(currentPostId);
+      Meteor.Router.to('postsList');
+    }
+  }
+});
