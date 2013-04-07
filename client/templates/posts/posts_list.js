a6f842a2037409ed811cfc62e3d76ccac43c5f15
Tom Coleman <tom@thesnail.org>
2013-04-07 17:48:06 +1000

    Wired collection into `postsList` template
    chapter4-3

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,17 +1,5 @@
-var postsData = [
-  {
-    title: 'Introducing Telescope',
-    url: 'http://sachagreif.com/introducing-telescope/'
-  }, 
-  {
-    title: 'Meteor',
-    url: 'http://meteor.com'
-  }, 
-  {
-    title: 'The Meteor Book',
-    url: 'http://themeteorbook.com'
-  }
-];
 Template.postsList.helpers({
-  posts: postsData
+  posts: function() {
+    return Posts.find();
+  }
 });
