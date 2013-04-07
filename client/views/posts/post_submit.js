2973a6b64f3c948fe2602305c691c6d119932d9c
Tom Coleman <tom@thesnail.org>
2013-04-07 19:25:58 +1000

    Use a method to submit the post.

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -8,7 +8,11 @@ Template.postSubmit.events({
       message: $(event.target).find('[name=message]').val()
     }
     
-    post._id = Posts.insert(post);
-    Meteor.Router.to('postPage', post);
+    Meteor.call('post', post, function(error, id) {
+      if (error)
+        return alert(error.reason);
+        
+      Meteor.Router.to('postPage', id);
+    });
   }
 });
