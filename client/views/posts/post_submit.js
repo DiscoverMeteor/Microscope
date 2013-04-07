2b6e6b3791d8cc5be07eceede999851c7e6f728e
Tom Coleman <tom@thesnail.org>
2013-04-07 18:40:48 +1000

    Added a submit post page and linked to it in the header.
    chapter7-1

---
+++ client/views/posts/post_submit.js
@@ -0,0 +1,14 @@
+Template.postSubmit.events({
+  'submit form': function(e) {
+    e.preventDefault();
+    
+    var post = {
+      url: $(e.target).find('[name=url]').val(),
+      title: $(e.target).find('[name=title]').val(),
+      message: $(e.target).find('[name=message]').val()
+    }
+    
+    post._id = Posts.insert(post);
+    Router.go('postPage', post);
+  }
+});
