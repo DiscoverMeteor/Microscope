72cf9111d8e993271c8b8061c7a9647bf9d99f91
Tom Coleman <tom@thesnail.org>
2013-04-07 23:14:36 +1000

    Added edit posts form
    chapter8-1

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -1,4 +1,7 @@
 Template.postItem.helpers({
+  ownPost: function() {
+    return this.userId == Meteor.userId();
+  },
   domain: function() {
     var a = document.createElement('a');
     a.href = this.url;
