5348bd6cd10402878f82f10e6162fac6adb6574c
Tom Coleman <tom@thesnail.org>
2013-04-08 14:34:51 +1000

    Made a simple publication/subscription for comments.
    chapter10-4

--- server/publications.js
+++ server/publications.js
@@ -2,6 +2,7 @@ Meteor.publish('posts', function() {
   return Posts.find();
 });
 
-Meteor.publish('comments', function() {
-  return Comments.find();
-})
+Meteor.publish('comments', function(postId) {
+  check(postId, String);
+  return Comments.find({postId: postId});
+});
