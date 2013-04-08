697b71dd6d900fee533ff7ce18c759ab9adb531c
Tom Coleman <tom@thesnail.org>
2013-04-08 12:04:19 +1000

    Clear errors after 3 seconds.
    chapter9-3

--- client/templates/includes/errors.js
+++ client/templates/includes/errors.js
@@ -3,3 +3,10 @@ Template.errors.helpers({
     return Errors.find();
   }
 });
+
+Template.error.rendered = function() {
+  var error = this.data;
+  Meteor.setTimeout(function () {
+    Errors.remove(error._id);
+  }, 3000);
+};
