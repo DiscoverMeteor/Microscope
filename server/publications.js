f4bfe2716614aa09116757b39a64acb449c94f1d
Tom Coleman <tom@thesnail.org>
2013-04-08 14:58:49 +1000

    Added basic notifications collection.
    And create a notification when a comment is created.
    
    chapter11-1

--- server/publications.js
+++ server/publications.js
@@ -6,3 +6,7 @@ Meteor.publish('comments', function(postId) {
   check(postId, String);
   return Comments.find({postId: postId});
 });
+
+Meteor.publish('notifications', function() {
+  return Notifications.find();
+})
