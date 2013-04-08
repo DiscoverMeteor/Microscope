cd437683e180b449dd03a5b0d0241d0f94045332
Tom Coleman <tom@thesnail.org>
2013-04-08 13:13:03 +1000

    Created a form to submit comments.
    chapter10-3

---
+++ client/templates/comments/comment_submit.js
@@ -0,0 +1,38 @@
+Template.commentSubmit.onCreated(function() {
+  Session.set('commentSubmitErrors', {});
+});
+
+Template.commentSubmit.helpers({
+  errorMessage: function(field) {
+    return Session.get('commentSubmitErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('commentSubmitErrors')[field] ? 'has-error' : '';
+  }
+});
+
+Template.commentSubmit.events({
+  'submit form': function(e, template) {
+    e.preventDefault();
+    
+    var $body = $(e.target).find('[name=body]');
+    var comment = {
+      body: $body.val(),
+      postId: template.data._id
+    };
+    
+    var errors = {};
+    if (! comment.body) {
+      errors.body = "Please write some content";
+      return Session.set('commentSubmitErrors', errors);
+    }
+    
+    Meteor.call('commentInsert', comment, function(error, commentId) {
+      if (error){
+        throwError(error.reason);
+      } else {
+        $body.val('');
+      }
+    });
+  }
+});
