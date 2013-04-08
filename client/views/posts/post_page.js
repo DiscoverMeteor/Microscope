3b7a45851986134d454761fbe1bd1325b9fea47d
Tom Coleman <tom@thesnail.org>
2013-04-08 13:03:27 +1000

    Display comments on `postPage`.

--- client/views/posts/post_page.js
+++ client/views/posts/post_page.js
@@ -1,5 +1,8 @@
 Template.postPage.helpers({
   currentPost: function() {
     return Posts.findOne(Session.get('currentPostId'));
+  },
+  comments: function() {
+    return Comments.find({postId: this._id});
   }
 });
