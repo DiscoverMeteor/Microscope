bc685c56c6e9637c197649e77542f0cf40ef06d9
Tom Coleman <tom@thesnail.org>
2013-04-08 22:29:24 +1000

    Fade items in when they are drawn.
    chapter14-2

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,5 +1,11 @@
 Template.postsList.rendered = function () {
   this.find('.wrapper')._uihooks = {
+    insertElement: function (node, next) {
+      $(node)
+        .hide()
+        .insertBefore(next)
+        .fadeIn();
+    },
     moveElement: function (node, next) {
       var $node = $(node), $next = $(next);
       var oldTop = $node.offset().top;
@@ -34,6 +40,11 @@ Template.postsList.rendered = function () {
       // reset everything to 0, animated
       $(node).addClass('animate').css('top', 0);
       $inBetween.addClass('animate').css('top', 0);
+    },
+    removeElement: function(node) {
+      $(node).fadeOut(function() {
+        $(this).remove();
+      });
     }
   }
 }
