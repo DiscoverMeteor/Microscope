fa348634674f23d872f994758f46b477d60ef7bb
Tom Coleman <tom@thesnail.org>
2013-04-08 16:36:48 +1000

    Added enough posts that pagination is necessary.
    chapter12-1

--- server/fixtures.js
+++ server/fixtures.js
@@ -54,4 +54,15 @@ if (Posts.find().count() === 0) {
     submitted: new Date(now - 12 * 3600 * 1000),
     commentsCount: 0
   });
+  
+  for (var i = 0; i < 10; i++) {
+    Posts.insert({
+      title: 'Test post #' + i,
+      author: sacha.profile.name,
+      userId: sacha._id,
+      url: 'http://google.com/?q=test-' + i,
+      submitted: new Date(now - i * 3600 * 1000 + 1),
+      commentsCount: 0
+    });
+  }
 }
