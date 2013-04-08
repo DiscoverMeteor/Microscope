9769d907a49018c1966e83f9fb8576ed2fca84f0
Tom Coleman <tom@thesnail.org>
2013-04-08 17:11:28 +1000

    Added two publications for posts, and pages to display them.
    chapter13-5

--- client/main.js
+++ client/main.js
@@ -1,4 +1,5 @@
-postsHandle = Meteor.subscribeWithPagination('newPosts', 10);
+newPostsHandle = Meteor.subscribeWithPagination('newPosts', 10);
+bestPostsHandle = Meteor.subscribeWithPagination('bestPosts', 10);
 
 Deps.autorun(function() {
   Meteor.subscribe('singlePost', Session.get('currentPostId'));
