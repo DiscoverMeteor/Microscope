e4cdc474c87e309631d21c26465e4cecc9a70f81
Tom Coleman <tom@thesnail.org>
2013-04-08 16:45:18 +1000

    Use the paginated subscription to step through posts.

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,5 +1,19 @@
 Template.postsList.helpers({
   posts: function() {
-    return Posts.find({}, {sort: {submitted: -1}});
+    return Posts.find({}, {sort: {submitted: -1}, limit: postsHandle.limit()});
+  },
+  postsReady: function() {
+    return postsHandle.ready();
+  },
+  allPostsLoaded: function() {
+    return postsHandle.ready() &&  
+      Posts.find().count() < postsHandle.loaded();
+  }
+});
+
+Template.postsList.events({
+  'click .load-more': function(event) {
+    event.preventDefault();
+    postsHandle.loadNextPage();
   }
 });
