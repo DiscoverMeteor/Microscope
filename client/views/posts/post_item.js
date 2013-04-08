c8f90a9150c27cd70c6669ff923e0bb977467036
Tom Coleman <tom@thesnail.org>
2013-04-08 22:20:38 +1000

    Added post reordering animation.

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -17,6 +17,30 @@ Template.postItem.helpers({
   }
 });
 
+Template.postItem.rendered = function(){
+  // animate post from previous position to new position
+  var instance = this;
+  var rank = instance.data._rank;
+  var $this = $(this.firstNode);
+  var postHeight = 80;
+  var newPosition = rank * postHeight;
+ 
+  // if element has a currentPosition (i.e. it's not the first ever render)
+  if (typeof(instance.currentPosition) !== 'undefined') {
+    var previousPosition = instance.currentPosition;
+    // calculate difference between old position and new position and send element there
+    var delta = previousPosition - newPosition;
+    $this.css("top", delta + "px");
+  }
+  
+  // let it draw in the old position, then..
+  Meteor.defer(function() {
+    instance.currentPosition = newPosition;
+    // bring element back to its new original position
+    $this.css("top",  "0px");
+  }); 
+};
+
 Template.postItem.events({
   'click .upvotable': function(e) {
     e.preventDefault();
