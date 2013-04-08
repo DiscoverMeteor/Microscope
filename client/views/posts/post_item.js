03ec3b84f36b03944227100895793d53b603ac13
Tom Coleman <tom@thesnail.org>
2013-04-08 22:29:24 +1000

    Fade items in when they are drawn.

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -24,20 +24,23 @@ Template.postItem.rendered = function(){
   var $this = $(this.firstNode);
   var postHeight = 80;
   var newPosition = rank * postHeight;
- 
+  
   // if element has a currentPosition (i.e. it's not the first ever render)
   if (typeof(instance.currentPosition) !== 'undefined') {
     var previousPosition = instance.currentPosition;
     // calculate difference between old position and new position and send element there
     var delta = previousPosition - newPosition;
     $this.css("top", delta + "px");
+  } else {
+    // it's the first ever render, so hide element
+    $this.addClass("invisible");
   }
   
   // let it draw in the old position, then..
   Meteor.defer(function() {
     instance.currentPosition = newPosition;
     // bring element back to its new original position
-    $this.css("top",  "0px");
+    $this.css("top",  "0px").removeClass("invisible");
   }); 
 };
 
