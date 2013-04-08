2fd67f521285310d4eec541d8264ba668418df90
Tom Coleman <tom@thesnail.org>
2013-04-08 17:11:28 +1000

    Added two publications for posts, and pages to display them.

--- server/publications.js
+++ server/publications.js
@@ -2,6 +2,10 @@ Meteor.publish('newPosts', function(limit) {
   return Posts.find({}, {sort: {submitted: -1}, limit: limit});
 });
 
+Meteor.publish('bestPosts', function(limit) {
+  return Posts.find({}, {sort: {votes: -1, submitted: -1}, limit: limit});
+});
+
 Meteor.publish('singlePost', function(id) {
   return id && Posts.find(id);
 });
