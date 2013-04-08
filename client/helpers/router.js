33cf5981d18f8cacc426d08e1600cac5067a792a
Tom Coleman <tom@thesnail.org>
2013-04-08 17:11:28 +1000

    Added two publications for posts, and pages to display them.

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -1,5 +1,7 @@
 Meteor.Router.add({
-  '/': 'postsList',
+  '/': {to: 'newPosts', as: 'home'},
+  '/best': 'bestPosts',
+  '/new': 'newPosts',
   
   '/posts/:_id': {
     to: 'postPage', 
--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,19 +1,37 @@
+Template.newPosts.helpers({
+  options: function() {
+    return {
+      sort: {submitted: -1},
+      handle: newPostsHandle
+    }
+  }
+});
+
+Template.bestPosts.helpers({
+  options: function() {
+    return {
+      sort: {votes: -1, submitted: -1},
+      handle: bestPostsHandle
+    }
+  }
+});
+
 Template.postsList.helpers({
   posts: function() {
-    return Posts.find({}, {sort: {submitted: -1}, limit: postsHandle.limit()});
+    return Posts.find({}, {sort: this.sort, limit: this.handle.limit()});
   },
   postsReady: function() {
-    return postsHandle.ready();
+    return this.handle.ready();
   },
   allPostsLoaded: function() {
-    return postsHandle.ready() &&  
-      Posts.find().count() < postsHandle.loaded();
+    return this.handle.ready() &&  
+      Posts.find().count() < this.handle.loaded();
   }
 });
 
 Template.postsList.events({
   'click .load-more': function(event) {
     event.preventDefault();
-    postsHandle.loadNextPage();
+    this.handle.loadNextPage();
   }
 });
