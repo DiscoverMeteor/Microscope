b1315a698b018b674066eef3aea33181b9cbf422
Tom Coleman <tom@thesnail.org>
2013-04-08 12:00:19 +1000

    Actually use the error reporting.
    chapter9-2

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -9,10 +9,16 @@ Template.postSubmit.events({
     }
     
     Meteor.call('post', post, function(error, id) {
-      if (error)
-        return alert(error.reason);
+      if (error) {
+        // display the error to the user
+        throwError(error.reason);
         
-      Meteor.Router.to('postPage', id);
+        // if the error is that the post already exists, take us there
+        if (error.error === 302)
+          Meteor.Router.to('postPage', error.details)
+      } else {
+        Meteor.Router.to('postPage', id);
+      }
     });
   }
 });
