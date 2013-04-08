82e1ad1c56d7974fca5b5754832cd236f16db5b6
Tom Coleman <tom@thesnail.org>
2013-04-08 17:18:23 +1000

    Added basic upvoting algorithm.
    chapter13-1

--- server/fixtures.js
+++ server/fixtures.js
@@ -18,7 +18,8 @@ if (Posts.find().count() === 0) {
     author: sacha.profile.name,
     url: 'http://sachagreif.com/introducing-telescope/',
     submitted: new Date(now - 7 * 3600 * 1000),
-    commentsCount: 2
+    commentsCount: 2,
+    upvoters: [], votes: 0
   });
   
   Comments.insert({
@@ -43,7 +44,8 @@ if (Posts.find().count() === 0) {
     author: tom.profile.name,
     url: 'http://meteor.com',
     submitted: new Date(now - 10 * 3600 * 1000),
-    commentsCount: 0
+    commentsCount: 0,
+    upvoters: [], votes: 0
   });
   
   Posts.insert({
@@ -52,7 +54,8 @@ if (Posts.find().count() === 0) {
     author: tom.profile.name,
     url: 'http://themeteorbook.com',
     submitted: new Date(now - 12 * 3600 * 1000),
-    commentsCount: 0
+    commentsCount: 0,
+    upvoters: [], votes: 0
   });
   
   for (var i = 0; i < 10; i++) {
@@ -62,7 +65,8 @@ if (Posts.find().count() === 0) {
       userId: sacha._id,
       url: 'http://google.com/?q=test-' + i,
       submitted: new Date(now - i * 3600 * 1000 + 1),
-      commentsCount: 0
+      commentsCount: 0,
+      upvoters: [], votes: 0
     });
   }
 }
