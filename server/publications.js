392c1f5c0acdf2e322e64b38117301d07e8aec36
Tom Coleman <tom@thesnail.org>
2013-04-08 16:39:56 +1000

    Added paginated subscription package, and ordered post by submitted date.
    chapter12-2

--- server/publications.js
+++ server/publications.js
@@ -1,5 +1,5 @@
-Meteor.publish('posts', function() {
-  return Posts.find();
+Meteor.publish('posts', function(limit) {
+  return Posts.find({}, {sort: {submitted: -1}, limit: limit});
 });
 
 Meteor.publish('comments', function(postId) {
