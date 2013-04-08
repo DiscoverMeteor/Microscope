f53e482050eee2a6c733b3aac0df1dfe3329544a
Tom Coleman <tom@thesnail.org>
2013-04-08 11:42:11 +1000

    Only allow changing certain fields of posts.

--- collections/posts.js
+++ collections/posts.js
@@ -5,6 +5,12 @@ Posts.allow({
   remove: ownsDocument
 });
 
+Posts.deny({
+  update: function(userId, post, fieldNames) {
+    // may only edit the following three fields:
+    return (_.without(fieldNames, 'url', 'title').length > 0);
+  }
+});
 
 Meteor.methods({
   post: function(postAttributes) {
