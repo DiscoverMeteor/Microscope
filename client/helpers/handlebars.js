390089567b6a2f887bd5f2ccae898b21162fe65a
Tom Coleman <tom@thesnail.org>
2013-04-08 16:57:19 +1000

    Added pluralize helper to format text better.
    chapter13-3

---
+++ client/helpers/handlebars.js
@@ -0,0 +1,8 @@
+Handlebars.registerHelper('pluralize', function(n, thing) {
+  // fairly stupid pluralizer
+  if (n === 1) {
+    return '1 ' + thing;
+  } else {
+    return n + ' ' + thing + 's';
+  }
+});
