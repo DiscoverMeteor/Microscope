7ee9b2261b1569137a4da76335d68d583a500224
Tom Coleman <tom@thesnail.org>
2013-04-10 13:57:55 +1000

    Created basic errors package and linked it in.
    sidebar9-5-1

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -27,11 +27,11 @@ Template.postSubmit.events({
     Meteor.call('postInsert', post, function(error, result) {
       // display the error to the user and abort
       if (error)
-        return throwError(error.reason);
+        return Errors.throw(error.reason);
       
       // show this result but route anyway
       if (result.postExists)
-        throwError('This link has already been posted');
+        Errors.throw('This link has already been posted');
       
       Router.go('postPage', {_id: result._id});  
     });
---
+++ packages/tmeasday:errors/package.js
@@ -0,0 +1,16 @@
+Package.describe({
+  name: "tmeasday:errors",
+  summary: "A pattern to display application errors to the user",
+  version: "1.0.0"
+});
+
+Package.onUse(function (api, where) {
+  api.versionsFrom('0.9.0');
+  
+  api.use(['minimongo', 'mongo-livedata', 'templating'], 'client');
+
+  api.addFiles(['errors.js', 'errors_list.html', 'errors_list.js'], 'client');
+  
+  if (api.export) 
+    api.export('Errors');
+});
