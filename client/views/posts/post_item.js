88199058afc58f400679eda231c8d9990223e702
Tom Coleman <tom@thesnail.org>
2013-04-10 22:05:27 +1000

    Denormalized the number of comments into the post.
    chapter10-5

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -6,8 +6,5 @@ Template.postItem.helpers({
     var a = document.createElement('a');
     a.href = this.url;
     return a.hostname;
-  },
-  commentsCount: function() {
-    return Comments.find({postId: this._id}).count();
   }
 });
