87e51cea7a7d78bb39f841d6472a9e8520b46640
Tom Coleman <tom@thesnail.org>
2013-05-05 18:24:02 -0700

    Use a single post subscription to ensure that we can always see the right post.
    chapter12-4

--- client/main.js
+++ client/main.js
@@ -1,6 +1,8 @@
-postsHandle = Meteor.subscribeWithPagination('posts', 10);
+postsHandle = Meteor.subscribeWithPagination('newPosts', 10);
 
 Deps.autorun(function() {
+  Meteor.subscribe('singlePost', Session.get('currentPostId'));
+  
   Meteor.subscribe('comments', Session.get('currentPostId'));
 })
 
--- server/publications.js
+++ server/publications.js
@@ -1,6 +1,11 @@
-Meteor.publish('posts', function(limit) {
+Meteor.publish('newPosts', function(limit) {
   return Posts.find({}, {sort: {submitted: -1}, limit: limit});
 });
+
+Meteor.publish('singlePost', function(id) {
+  return id && Posts.find(id);
+});
+
 
 Meteor.publish('comments', function(postId) {
   return Comments.find({postId: postId});
