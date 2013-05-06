422314b30e6cb5749e0878a184b827c3886453d7
Tom Coleman <tom@thesnail.org>
2013-05-05 18:14:03 -0700

    Grey out upvote link when not logged in / already voted.

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -6,11 +6,19 @@ Template.postItem.helpers({
     var a = document.createElement('a');
     a.href = this.url;
     return a.hostname;
+  },
+  upvotedClass: function() {
+    var userId = Meteor.userId();
+    if (userId && !_.include(this.upvoters, userId)) {
+      return 'btn-primary upvoteable';
+    } else {
+      return 'disabled';
+    }
   }
 });
 
 Template.postItem.events({
-  'click .upvote': function(event) {
+  'click .upvoteable': function(event) {
     event.preventDefault();
     Meteor.call('upvote', this._id);
   }
