cfeb935f6a0702e737bdd8e7d5c00d15d195282d
Sacha Greif <sacha357@gmail.com>
2013-06-11 07:10:13 +0900

    Added Intercom code snippet
    chapter17-1

---
+++ client/main.js
@@ -0,0 +1,7 @@
+window.intercomSettings = {
+  // TODO: The current logged in user's email address.
+  email: "john.doe@example.com",
+  // TODO: The current logged in user's sign-up date as a Unix timestamp.
+  created_at: 1234567890,
+  app_id: "9b593f5995da6d2bd8f418754c0411d55201258a"
+};
