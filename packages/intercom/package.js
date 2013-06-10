05014bd7c301c0117cf2a3cf5ce5c3a9a3ad27f4
Sacha Greif <sacha357@gmail.com>
2013-06-11 07:10:13 +0900

    Added Intercom code snippet
    chapter17-1

---
+++ packages/intercom/package.js
@@ -0,0 +1,9 @@
+Package.describe({
+  name: 'intercom',
+  summary: "Intercom package",
+  version: '1.0.0'
+});
+Package.on_use(function (api) {
+  api.versionsFrom('0.9.4');
+  api.add_files('intercom_loader.js', 'client');
+});
