1289bef252ca9c5d309b86bf6ccb56c2a15cba96
Sacha Greif <sacha357@gmail.com>
2013-06-11 09:27:47 +0900

    Sending custom user data.

--- client/main.js
+++ client/main.js
@@ -9,10 +9,16 @@ Deps.autorun(function() {
 
 Meteor.subscribe('notifications');
 
-window.intercomSettings = {
-  // TODO: The current logged in user's email address.
-  email: "john.doe@example.com",
-  // TODO: The current logged in user's sign-up date as a Unix timestamp.
-  created_at: 1234567890,
-  app_id: "9b593f5995da6d2bd8f418754c0411d55201258a"
-};
+Meteor.subscribe('currentUser');
+
+Deps.autorun(function(){
+  if (Meteor.user() && !Meteor.loggingIn()) {
+    var intercomSettings = {
+      email: Meteor.user().emails[0].address,
+      created_at: Math.round(Meteor.user().createdAt/1000),
+      user_name: Meteor.user().username,
+      app_id: "k20iexvc"
+    };
+    Intercom('boot', intercomSettings);
+  }
+});
