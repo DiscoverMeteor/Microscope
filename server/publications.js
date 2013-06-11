c1d7a298359ef85ac760ec1a079e0753c40ead2c
Sacha Greif <sacha357@gmail.com>
2013-06-11 09:27:47 +0900

    Sending custom user data.

--- server/publications.js
+++ server/publications.js
@@ -10,11 +10,14 @@ Meteor.publish('singlePost', function(id) {
   return id && Posts.find(id);
 });
 
-
 Meteor.publish('comments', function(postId) {
   return Comments.find({postId: postId});
 });
 
 Meteor.publish('notifications', function() {
   return Notifications.find({userId: this.userId});
+});
+
+Meteor.publish('currentUser', function() {
+  return Meteor.users.find(this.userId, {fields: {createdAt: 1}});
 });
