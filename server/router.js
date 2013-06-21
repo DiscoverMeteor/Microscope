93a1dd9f74289624f9388f26b903fc2ba0e66a4a
Tom Coleman <tom@thesnail.org>
2013-06-21 14:39:26 +1000

    Created RSS package, use it to create a rudimentary feed.

--- server/router.js
+++ server/router.js
@@ -1,3 +1,10 @@
 Meteor.Router.add({
-  '/feed.xml': "hello\n"
+  '/feed.xml': function() {
+    var feed = new RSS({
+      title: "New Microscope Posts",
+      description: "The latest posts from Microscope, the smallest news aggregator."
+    });
+    
+    return feed.xml();
+  }
 });
