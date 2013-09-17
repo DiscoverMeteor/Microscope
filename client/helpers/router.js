5969014805ea4808cd53aac25a3cd2186682384a
Tom Coleman <tom@thesnail.org>
2013-09-17 16:15:42 +1000

    IronRouter screencast code.

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -1,35 +1,61 @@
-Meteor.Router.add({
-  '/': {to: 'newPosts', as: 'home'},
-  '/best': 'bestPosts',
-  '/new': 'newPosts',
-  
-  '/posts/:_id': {
-    to: 'postPage', 
-    and: function(id) { Session.set('currentPostId', id); }
-  },
-  
-  '/posts/:_id/edit': {
-    to: 'postEdit', 
-    and: function(id) { Session.set('currentPostId', id); }    
-  },
-  
-  '/submit': 'postSubmit'
+Router.configure({
+  layout: 'layout',
+  loadingTemplate: 'loading'
 });
 
-Meteor.Router.filters({
-  'requireLogin': function(page) {
-    if (Meteor.user())
-      return page;
-    else if (Meteor.loggingIn())
-      return 'loading';
-    else
-      return 'accessDenied';
-  },
-  'clearErrors': function(page) {
-    clearErrors();
-    return page;
-  }
+Router.map(function() {
+  this.route('home', {
+    path: '/', template: 'newPosts'
+  });
+  
+  this.route('bestPosts', {path: '/best'});
+  this.route('newPosts', {path: '/new'});
+  
+  this.route('postPage', {
+    path: '/posts/:_id',
+    waitOn: function() {
+      return Meteor.subscribe('singlePost', this.params._id);
+    },
+    data: function() {
+      return Posts.findOne(this.params._id);
+    }
+  });
+  
 });
 
-Meteor.Router.filter('requireLogin', {only: 'postSubmit'});
-Meteor.Router.filter('clearErrors');
+
+// Meteor.Router.add({
+//   '/': {to: 'newPosts', as: 'home'},
+//   '/best': 'bestPosts',
+//   '/new': 'newPosts',
+//   
+//   '/posts/:_id': {
+//     to: 'postPage', 
+//     and: function(id) { Session.set('currentPostId', id); }
+//   },
+//   
+//   '/posts/:_id/edit': {
+//     to: 'postEdit', 
+//     and: function(id) { Session.set('currentPostId', id); }    
+//   },
+//   
+//   '/submit': 'postSubmit'
+// });
+// 
+// Meteor.Router.filters({
+//   'requireLogin': function(page) {
+//     if (Meteor.user())
+//       return page;
+//     else if (Meteor.loggingIn())
+//       return 'loading';
+//     else
+//       return 'accessDenied';
+//   },
+//   'clearErrors': function(page) {
+//     clearErrors();
+//     return page;
+//   }
+// });
+// 
+// Meteor.Router.filter('requireLogin', {only: 'postSubmit'});
+// Meteor.Router.filter('clearErrors');
--- client/main.js
+++ client/main.js
@@ -2,8 +2,6 @@ newPostsHandle = Meteor.subscribeWithPagination('newPosts', 10);
 bestPostsHandle = Meteor.subscribeWithPagination('bestPosts', 10);
 
 Deps.autorun(function() {
-  Meteor.subscribe('singlePost', Session.get('currentPostId'));
-  
   Meteor.subscribe('comments', Session.get('currentPostId'));
 })
 
