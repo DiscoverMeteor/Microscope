e61856160ea71df86b19e4728d098bdf5943ad4e
Tom Coleman <tom@thesnail.org>
2013-11-26 19:47:49 +1100

    Use a single post subscription to ensure that we can always see the right post.
    chapter12-6

--- server/publications.js
+++ server/publications.js
@@ -6,6 +6,12 @@ Meteor.publish('posts', function(options) {
   return Posts.find({}, options);
 });
 
+Meteor.publish('singlePost', function(id) {
+  check(id, String);
+  return Posts.find(id);
+});
+
+
 Meteor.publish('comments', function(postId) {
   check(postId, String);
   return Comments.find({postId: postId});
