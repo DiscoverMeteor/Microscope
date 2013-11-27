07ac6cee87e3499a2a7078174ef6539544fa25af
Tom Coleman <tom@thesnail.org>
2013-11-27 14:30:09 +1100

    Added routes for post lists, and pages to display them.
    chapter13-5

--- client/views/posts/post_edit.js
+++ client/views/posts/post_edit.js
@@ -25,7 +25,7 @@ Template.postEdit.events({
     if (confirm("Delete this post?")) {
       var currentPostId = this._id;
       Posts.remove(currentPostId);
-      Router.go('postsList');
+      Router.go('home');
     }
   }
 });
