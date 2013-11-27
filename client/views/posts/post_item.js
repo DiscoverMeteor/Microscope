cfa0a33633605be31125d9e700dac59db00d53d4
Tom Coleman <tom@thesnail.org>
2013-11-27 14:34:16 +1100

    Added post reordering animation.
    chapter14-1

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -1,3 +1,6 @@
+var POST_HEIGHT = 80;
+var Positions = new Meteor.Collection(null);
+
 Template.postItem.helpers({
   ownPost: function() {
     return this.userId == Meteor.userId();
@@ -14,6 +17,24 @@ Template.postItem.helpers({
     } else {
       return 'disabled';
     }
+  },
+  attributes: function() {
+    var post = _.extend({}, Positions.findOne({postId: this._id}), this);
+    var newPosition = post._rank * POST_HEIGHT;
+    var attributes = {};
+    
+    if (! _.isUndefined(post.position)) {
+      var delta = post.position - newPosition;      
+      attributes.style = "top: " + delta + "px";
+      if (delta === 0)
+        attributes.class = "post animate"
+    }
+    
+    Meteor.setTimeout(function() {
+      Positions.upsert({postId: post._id}, {$set: {position: newPosition}})
+    });
+    
+    return attributes;
   }
 });
 
---
+++ client/views/posts/posts_list.js
@@ -0,0 +1,9 @@
+Template.postsList.helpers({
+  postsWithRank: function() {
+    this.posts.rewind();
+    return this.posts.map(function(post, index, cursor) {
+      post._rank = index;
+      return post;
+    });
+  }
+});
