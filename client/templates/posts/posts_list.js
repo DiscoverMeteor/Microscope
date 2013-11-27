8cf9ec0c0d2562df54719d5a976768fee16819f3
Tom Coleman <tom@thesnail.org>
2013-11-27 14:34:16 +1100

    Added post reordering animation.
    chapter14-1

---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,39 @@
+Template.postsList.rendered = function () {
+  this.find('.wrapper')._uihooks = {
+    moveElement: function (node, next) {
+      var $node = $(node), $next = $(next);
+      var oldTop = $node.offset().top;
+      var height = $(node).outerHeight(true);
+      
+      // find all the elements between next and node
+      var $inBetween = $(next).nextUntil(node);
+      if ($inBetween.length === 0)
+        $inBetween = $(node).nextUntil(next);
+      
+      // now put node in place
+      $(node).insertBefore(next);
+      
+      // measure new top
+      var newTop = $(node).offset().top;
+      
+      // move node *back* to where it was before
+      $(node)
+        .removeClass('animate')
+        .css('top', oldTop - newTop);
+      
+      // push every other element down (or up) to put them back
+      $inBetween.css('top', oldTop < newTop ? height : -1 * height)
+      $inBetween
+        .removeClass('animate')
+        .css('top', oldTop < newTop ? height : -1 * height)
+        
+      
+      // force a redraw
+      $(node).offset();
+      
+      // reset everything to 0, animated
+      $(node).addClass('animate').css('top', 0);
+      $inBetween.addClass('animate').css('top', 0);
+    }
+  }
+}
