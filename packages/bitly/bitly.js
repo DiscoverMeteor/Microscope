a261e91471e1cbf74656b0f79c253a9bb15f7a2f
Sacha Greif <sacha357@gmail.com>
2014-01-08 08:30:41 +0900

    Added getClicks method.
    chapter16-4

--- packages/bitly/bitly.js
+++ packages/bitly/bitly.js
@@ -21,3 +21,28 @@ Bitly.shortenURL = function(url){
     throw new Meteor.Error(500, "Bitly call failed with error: "+shortenResponse.status_txt);
   }
 }
+
+Bitly.getClicks = function(link){
+  if(!Meteor.settings.bitly)
+    throw new Meteor.Error(500, 'Please provide a Bitly token in Meteor.settings');
+
+  var statsResponse = Meteor.http.get(
+    "https://api-ssl.bitly.com/v3/link/clicks?", 
+    {
+      timeout: 5000,
+      params:{ 
+        "format": "json",
+        "access_token": Meteor.settings.bitly,
+        "link": link
+      }
+    }
+  );
+  if(statsResponse.data.status_code == 200)
+    return statsResponse.data.data.link_clicks
+}
+
+Meteor.methods({
+  'getBitlyClicks': function(link){
+    return Bitly.getClicks(link);
+  }
+})
