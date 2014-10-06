836124e965acc07d801f9f125dd1b6d9c4484811
Tom Coleman <tom@thesnail.org>
2014-10-06 12:01:43 +1100

    Throw error if you can’t upvote. #197

--- collections/posts.js
+++ collections/posts.js
@@ -56,12 +56,15 @@ Meteor.methods({
     if (!user)
       throw new Meteor.Error(401, "You need to login to upvote");
     
-    Posts.update({
+    var affected = Posts.update({
       _id: postId, 
       upvoters: {$ne: user._id}
     }, {
       $addToSet: {upvoters: user._id},
       $inc: {votes: 1}
     });
+    
+    if (! affected)
+      throw new Meteor.Error(422, "You weren't able to upvote that post");
   }
 });
