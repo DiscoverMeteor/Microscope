2244943e5efe159ff29f772badb943125b3c6063
Sacha Greif <sacha357@gmail.com>
2014-10-10 10:13:44 +0900

    clean up

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -17,8 +17,7 @@ Template.postSubmit.events({
     
     var post = {
       url: $(e.target).find('[name=url]').val(),
-      title: $(e.target).find('[name=title]').val(),
-      message: $(e.target).find('[name=message]').val()
+      title: $(e.target).find('[name=title]').val()
     }
     
     var errors = validatePost(post);
