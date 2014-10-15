4fe26067633eb9334ee09a9174f67285e4e1231b
Tom Coleman <tom@thesnail.org>
2014-10-15 16:51:16 +1100

    Validate post contents when editing
    chapter9-5

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -1,3 +1,16 @@
+Template.postEdit.created = function() {
+  Session.set('postEditErrors', {});
+}
+
+Template.postEdit.helpers({
+  errorMessage: function(field) {
+    return Session.get('postEditErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('postEditErrors')[field] ? 'has-error' : '';
+  }
+});
+
 Template.postEdit.events({
   'submit form': function(e) {
     e.preventDefault();
@@ -9,10 +22,14 @@ Template.postEdit.events({
       title: $(e.target).find('[name=title]').val()
     }
     
+    var errors = validatePost(postProperties);
+    if (errors.title || errors.url)
+      return Session.set('postEditErrors', errors);
+    
     Posts.update(currentPostId, {$set: postProperties}, function(error) {
       if (error) {
         // display the error to the user
-        throwError(error.reason);
+        alert(error.reason);
       } else {
         Router.go('postPage', {_id: currentPostId});
       }
