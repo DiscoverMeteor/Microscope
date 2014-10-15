c92904bed9083294839ad8927fb7b90b7794365f
Tom Coleman <tom@thesnail.org>
2014-10-15 16:33:40 +1100

    Enforce post URL uniqueness
    chapter7-7

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -12,6 +12,10 @@ Template.postSubmit.events({
       if (error)
         return alert(error.reason);
       
+      // show this result but route anyway
+      if (result.postExists)
+        alert('This link has already been posted');
+      
       Router.go('postPage', {_id: result._id});  
     });
   }
