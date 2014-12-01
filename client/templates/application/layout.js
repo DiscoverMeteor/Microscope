70beaf727ab5e9c5b2b60a3c4e72974aa28668c3
Tom Coleman <tom@thesnail.org>
2014-12-01 21:17:36 +1100

    Transition between pages by fading
    chapter14-3

---
+++ client/templates/application/layout.js
@@ -0,0 +1,15 @@
+Template.layout.onRendered(function() {
+  this.find('#main')._uihooks = {
+    insertElement: function(node, next) {
+      $(node)
+        .hide()
+        .insertBefore(next)
+        .fadeIn();
+    },
+    removeElement: function(node) {
+      $(node).fadeOut(function() {
+        $(this).remove();
+      });
+    }
+  }
+});
